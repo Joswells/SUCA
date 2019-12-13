7d1fc75a39e48a3c7e370e4559ef73ad7d2d9daf
Joswells <wagnerjr300@gmail.com>
2019-12-13 17:06:58 -0300

    serviço de login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,8 +2,14 @@ const Usuario = require('../models/Usuario')
 
 module.exports = {
     validacao(req, res) {
-        if(req.login == "admin" && req.senha == "root") {
-            return res.json({ code: 200, message: "Usuario existente" })
+        if (!req.body) {
+            return res.json({ status:404, message: "Usuário não existe" })
         }
+        const { email, senha } = req.body;
+
+        if(email == "admin" && senha == "root")
+            return res.json({ status: 200, message:"ok" })
+        else
+            return res.json({ status:404, message: "Usuário não existe" })
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const FuncionarioController = require('./controllers/funcionarioController');
 const entradaSaidaController = require('./controllers/entradaSaidaController');
 const FuncionariosPresentes = require('./controllers/presentesController');
+const LoginController = require('./controllers/loginController');
 
 const routes = express.Router();
+
+routes.post('/login', LoginController.validacao)
 
 routes.get('/funcionarios', FuncionarioController.index);
 routes.post('/funcionario/info', FuncionarioController.getOne)
